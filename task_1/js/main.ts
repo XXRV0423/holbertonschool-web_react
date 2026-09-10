2fc13ba3b99ea3bbc3f3996c388baf6a0ac8070d
XXRV0423 <xxavierrv@gmail.com>
2026-09-10 18:46:29 -0400

    feat: add StudentClass with constructor and instance interfaces
    Add StudentClassInterface describing the instance shape (firstName,
    lastName, workOnHomework, displayName) and StudentClassConstructor
    describing the class constructor signature. Implement StudentClass
    to satisfy both, with workOnHomework returning 'Currently working'
    and displayName returning the student's firstName.

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -57,3 +57,34 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
 }
 
 console.log(printTeacher('John', 'Doe'));
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student = new StudentClass('John', 'Doe');
+console.log(student.workOnHomework());
+console.log(student.displayName());
